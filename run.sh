282ea08e59aef6ccf5408237acdf3655e3866451
mike-trueh <mike.trueh@gmail.com>
2022-06-15 20:43:55 +0500

    fix: run script for some cases

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ docker-compose up -d
 
 docker-compose exec laravel.test composer install
 
+docker-compose down && docker-compose up -d
+
+wait
+
 ./vendor/bin/sail artisan key:generate
 
 ./vendor/bin/sail artisan migrate
